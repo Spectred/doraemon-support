0436ec041ce32255c057e69d006bf7a8ecf302e6
Spectred <1505073336@qq.com>
2019-09-11 19:00:16 +0800

    Use Spock For Unit Test

---
+++ doraemon-coffee/src/test/java/com/doraemon/StringLanguageUtilTest.groovy
@@ -0,0 +1,22 @@
+package com.doraemon
+
+import spock.lang.*
+
+import static com.doraemon.StringLanguageUtil.CHINESE
+import static com.doraemon.StringLanguageUtil.ENGLISH
+import static com.doraemon.StringLanguageUtil.OTHER
+
+class StringLanguageUtilTest extends Specification {
+
+
+    def "test get Language"() {
+        expect:
+        result == StringLanguageUtil.getLanguage(str)
+
+        where:
+        str       || result
+        "hello"   || ENGLISH
+        "哈喽"      || CHINESE
+        "哈喽world" || OTHER
+    }
+}
